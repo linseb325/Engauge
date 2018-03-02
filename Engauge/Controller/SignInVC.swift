825895cfcdd542bb0a8069348b0c3d643d148845
linseb325 <linseb325@gmail.com>
2018-03-02 16:09:55 -0600

    Added Firebase pods.

---
+++ Engauge/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  Engauge
+//
+//  Created by Brennan Linse on 3/2/18.
+//  Copyright © 2018 Brennan Linse. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
